5a6d0da96b5babc7f295cb0fe7de4ca166216d72
Ethan Neill <ethanneill@cs.ucsb.edu>
2022-03-08 02:00:27 -0800

    Done

--- heap.cpp
+++ heap.cpp
@@ -9,18 +9,76 @@ using std::cout;
 // Pushes a value into the heap, then ensures
 // the heap is correctly arranged
 void Heap::push(int value){
-  std::vector<int>::iterator it = vdata.begin();
-  while(value > *it){
-    next(it);
+  vdata.push_back(value);
+  int parent = (vdata.size() - 2) / 2;
+  int child = vdata.size() - 1;
+  int timer = 1;
+  while(timer == 1){
+    timer--;
+    if((vdata[parent] > value) && (parent != -1)){
+      vdata[child] = vdata[parent];
+      vdata[parent] = value;
+      child = parent;
+      parent = (parent - 1) / 2;
+      timer++;
+    }
   }
-  vdata.insert(it, value);
 }
 
 // Pops the minimum value off the heap
 // (but does not return it), then ensures
 // the heap is correctly arranged
 void Heap::pop(){
-  vdata.erase(vdata.begin());
+  vdata.front() = vdata.back();
+  vdata.back() = 0;
+  vdata.erase(vdata.end()-1);
+  int parent = 0;
+  int leftChild = 1;
+  int rightChild = 2;
+  int timer = 1;
+  while(timer == 1){
+    timer--;
+    if(vdata[leftChild] || vdata[rightChild]){
+      if(vdata[leftChild] && vdata[rightChild]){
+        if(vdata[leftChild] < vdata[rightChild]){
+          if(vdata[leftChild] < vdata[parent]){
+            int i = vdata[parent];
+            vdata[parent] = vdata[leftChild];
+            vdata[leftChild] = i;
+            parent = leftChild;
+            timer++;
+          }
+        }
+        else if(vdata[rightChild] < vdata[parent]){
+          int i = vdata[parent];
+          vdata[parent] = vdata[rightChild];
+          vdata[rightChild] = i;
+          parent = rightChild;
+          timer++;
+        }
+      }
+      else if(vdata[leftChild]){
+        if(vdata[leftChild] < vdata[parent]){
+          int i = vdata[parent];
+          vdata[parent] = vdata[leftChild];
+          vdata[leftChild] = i;
+          parent = leftChild;
+          timer++;
+        }
+      }
+      else if(vdata[rightChild]){
+        if(vdata[rightChild] < vdata[parent]){
+          int i = vdata[parent];
+          vdata[parent] = vdata[rightChild];
+          vdata[rightChild] = i;
+          parent = rightChild;
+          timer++;
+        }
+      }
+    leftChild = (2 * parent) + 1;
+    rightChild = (2 * parent) + 2;
+    }
+  }
 }
 
 // Returns the minimum element in the heap
@@ -37,4 +95,3 @@ bool Heap::empty(){
     return false;
   }
 }
-    
